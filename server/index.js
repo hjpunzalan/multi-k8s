aa2f0f25f74ff623a82ea173039d8b464101cb9e
hjpunzalan <hj.punzalan@hotmail.com>
2020-02-20 17:07:25 +0800

    redis initial setup on server

--- server/index.js
+++ server/index.js
@@ -23,4 +23,15 @@ pgClient.on('error', ()=> console.log('Lost PG connection'))
 
 
 pgClient.query('CREATE TABLE IF NOT EXISTS values (number INT)')
-.catch(err => console.log(err))
+    .catch(err => console.log(err))
+
+
+// Redis client setup
+const redis = require('redis');
+const redisClient = redis.createClient({
+    host: keys.redisHost,
+    port: keys.redisPort,
+    retry_strategy: () => 1000
+})
+
+const redisPublisher = redisClient.duplicate();
