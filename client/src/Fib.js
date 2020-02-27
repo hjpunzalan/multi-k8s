71764781bbd6941af6d8d1bf8d42977c78bff48b
hjpunzalan <hj.punzalan@hotmail.com>
2020-02-27 14:43:26 +0800

    push client

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -66,7 +66,7 @@ export class Fib extends Component {
 					<button>Submit</button>
 				</form>
 
-				<h3>Indexes I have seen:</h3>
+				<h3>Indexes I have seen :</h3>
 				{this.renderSeenIndexes()}
 				<h3>Calculated Values:</h3>
 				{this.renderValues()}
